82cb28e76bba46671cfcd980de7c10e0aa9280cc
Harsha Dalali <dallali.harsha@gmail.com>
2026-03-16 16:43:25 -0700

    Deploy to free domain (#12)

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        courses: resolve(__dirname, 'courses.html'),
+        freeClass: resolve(__dirname, 'free-class.html'),
+        login: resolve(__dirname, 'login.html'),
+        studentCorner: resolve(__dirname, 'student-corner.html'),
+        videos: resolve(__dirname, 'videos.html'),
+      },
+    },
+  },
+})
